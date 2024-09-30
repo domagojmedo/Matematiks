8102b3eb0be43fa6944a9b624affc6024f3133f2
Domagoj Medo <domagoj.medo@moberg.hr>
2024-09-30 09:13:38 +0200

    Add project files.

---
+++ Matematiks/Matematiks.Shared/Services/IFormFactor.cs
@@ -0,0 +1,8 @@
+namespace Matematiks.Shared.Services
+{
+    public interface IFormFactor
+    {
+        public string GetFormFactor();
+        public string GetPlatform();
+    }
+}
---
+++ Matematiks/Matematiks.Web/Program.cs
@@ -0,0 +1,35 @@
+using Matematiks.Shared.Services;
+using Matematiks.Web.Components;
+using Matematiks.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+// Add device-specific services used by the Matematiks.Shared project
+builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(
+        typeof(Matematiks.Shared._Imports).Assembly,
+        typeof(Matematiks.Web.Client._Imports).Assembly);
+
+app.Run();
